88360033e88af44666752d42dbbe85527e3e172e
Rohan Bendre <rohanbendre@gmail.com>
2016-09-07 11:33:30 -0500

    Update taskplannerdemo.py

--- src/taskplannerdemo.py
+++ src/taskplannerdemo.py
@@ -3,6 +3,6 @@ import scheduler
 from scheduler import task_scheduler
 
 t = task_scheduler.TaskScheduler()
-t.getTaskList("/Users/rohanbendre/Desktop/user.yaml")
-t.getProcessorList("/Users/rohanbendre/Desktop/processors.yaml")
+t.getTaskList("path_to_yaml_file_for_tasks")
+t.getProcessorList("path_to_yaml_file_for_processors")
 t.executeTasks()
